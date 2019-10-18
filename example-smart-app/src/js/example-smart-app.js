6c5eae97d4439b3b39caeefcb834eced2c4701bd
Timothy Ho <timbvho@gmail.com>
2019-10-18 10:44:24 -0500

    weight 2

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -49,7 +49,7 @@
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
-          p.weight = weight;
+          p.weight = getQuantityValueAndUnit(weight[0]);
           p.height = getQuantityValueAndUnit(height[0]);
 
           if (typeof systolicbp != 'undefined')  {
